5cb1c603c86ced6a297c0a42abc98f9a010eccdb
Jake Swensen <jake@swensen.io>
2022-02-09 23:56:22 -0600

    fix: GenCtestWithGtest: fix gtest build

--- GenCtestWithGtest.cmake
+++ GenCtestWithGtest.cmake
@@ -6,8 +6,9 @@ set(__gen_ctest_with_gtest INCLUDED)
 if (HUNTER_ENABLED)
     message(STATUS "GTest: use Hunter version")
     hunter_add_package(GTest)
-    find_package(GTest CONFIG REQUIRED)
-    set(GTEST_LINK GTest::main)
+    find_package(GTest CONFIG REQUIRED HINTS ${GTEST_ROOT})
+    set(GTEST_LINK GTest::gtest_main GTest::gmock)
+    message(STATUS "GTEST_ROOT: ${GTEST_ROOT}")
 else()
     message(STATUS "GTest: use system version")
     find_library(gtest gtest.so)
@@ -21,11 +22,10 @@ function(gen_ctest_with_gtest)
     set(multi_value_args TEST_SOURCE TEST_LINKS EXTRA_INCLUDE TEST_ARGS)
     cmake_parse_arguments(gen_ctest_with_gtest "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
-
     include_directories(${gen_ctest_with_gtest_EXTRA_INCLUDE})
 
     # Set short aliases
-    set(src_links ${gen_ctest_with_gtest_TEST_LINKS} pthread ${GTEST_LINK})
+    set(src_links ${gen_ctest_with_gtest_TEST_LINKS} pthread)
     set(test_src ${gen_ctest_with_gtest_TEST_SOURCE})
     set(exe_name test-${PROJECT_NAME}-${gen_ctest_with_gtest_COMPONENT_NAME})
 
@@ -38,7 +38,7 @@ function(gen_ctest_with_gtest)
 
     # Generate test executable
     add_executable(${exe_name} ${test_src})
-    target_link_libraries(${exe_name} ${src_links})
+    target_link_libraries(${exe_name} PRIVATE ${src_links} ${GTEST_LINK})
     add_test(${exe_name} ${exe_name} ${test_args})
 
     install(TARGETS ${exe_name}
